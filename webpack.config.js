db9414ae985a0209505b8d5b2973cb2e1081e7e0
beitaz <beitaz@icloud.com>
2018-02-08 15:45:51 +0800

    添加 Webpack 配置

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    },
+    extensions: ['.js', '.jsx', '.json', '.css', '.scss'],
+    modules: ['node_modules']
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: ['env', 'react']
+        }
+      }, {
+        loader: 'eslint-loader',
+        options: {
+          enforce: 'pre'
+        }
+      }]
+    },{
+      test: /\.s?css/,
+      exclude: /node_modules/,
+      use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader']
+    }]
+  },
+  plugins: [
+    require('autoprefixer'),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      title: 'Blocks',
+      favicon: './public/favicon.ico'
+    })
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    compress: true,
+    // host: 'localhost',
+    // port: 8081,
+    // proxy: 'http://localhost:8087',
+    open: 'Safari', // true 或者 浏览器名称 'Google Chrome', 'Firefox', 'Safari'
+    // headers: { // 为所有响应添加额外的头信息
+    //   'X-Custom-Foo': 'bar'
+    // }
+  }
+};
