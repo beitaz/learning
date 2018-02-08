ed08905a9a7f9efbf84a2c2067b4945a79d6146f
beitaz <beitaz@icloud.com>
2018-02-08 16:14:44 +0800

    添加 webpack 配置

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   module: {
     rules: [{
       test: /\.jsx?$/,
-      exclude: /node_modules/,
+      include: path.resolve(__dirname, 'src'),
       use: [{
         loader: 'babel-loader',
         options: {
@@ -39,17 +39,17 @@ module.exports = {
     require('autoprefixer'),
     new HtmlWebpackPlugin({
       template: './public/index.html',
-      title: 'Blocks',
       favicon: './public/favicon.ico'
     })
   ],
+  devtool: 'cheap-module-eval-source-map',
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
     compress: true,
     // host: 'localhost',
     // port: 8081,
     // proxy: 'http://localhost:8087',
-    open: 'Safari', // true 或者 浏览器名称 'Google Chrome', 'Firefox', 'Safari'
+    open: 'Google Chrome', // true 或者 浏览器名称 'Google Chrome', 'Firefox', 'Safari'
     // headers: { // 为所有响应添加额外的头信息
     //   'X-Custom-Foo': 'bar'
     // }
